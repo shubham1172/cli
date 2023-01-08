36ce9db080036928ffd85ce233c4cf69fd7b40de
Shubham Sharma <shubhash@microsoft.com>
2023-01-08 23:55:05 +0530

    WIP changes
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- pkg/standalone/subscribe.go
+++ pkg/standalone/subscribe.go
@@ -13,8 +13,59 @@ limitations under the License.
 
 package standalone
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/phayes/freeport"
+)
+
+const (
+	subscribeHttpRoute = "/dapr/subscribe"
+)
+
+type subscription struct {
+	PubsubName string                 `json:"pubsubname"`
+	Topic      string                 `json:"topic"`
+	Route      string                 `json:"route"`
+	Metadata   map[string]interface{} `json:"metadata"`
+}
+
+// startSubscribeServer starts a web server on a random port
+// and registers the required HTTP handlers.
+func startSubscribeServer(s subscription) (int, error) {
+	port, err := freeport.GetFreePort()
+	if err != nil {
+		return -1, err
+	}
+
+	http.HandleFunc(subscribeHttpRoute, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode([]subscription{s})
+	})
+
+	go http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	return port, nil
+}
+
 // Subscribe to a topic in a pubsub and receive messages.
 func (s *Standalone) Subscribe(appID, pubsubName, topic, socket string, metadata, routes map[string]interface{}) error {
-	// TODO: Implement this method.
+	strRoutes, err := json.Marshal(routes)
+	if err != nil {
+		return err
+	}
+
+	sub := subscription{
+		PubsubName: pubsubName,
+		Topic:      topic,
+		Metadata:   metadata,
+		Route:      string(strRoutes),
+	}
+
+	_, err = startSubscribeServer(sub)
+	if err != nil {
+		return err
+	}
 	return nil
 }
